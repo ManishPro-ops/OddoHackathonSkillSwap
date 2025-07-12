a059b85e2bc833bdddd1cda140325b8966cc2789
Bhavin Suthar <bhtechie9911@gmail.com>
2025-07-12 16:02:18 +0530

    Request_Page_Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/Login";
-import SignUp from "./pages/SignUp";
+// import HomePage from "./pages/HomePage";
+// import { Route, Routes } from "react-router-dom";
+// import LoginPage from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+// import RequestPage from "./pages/RequestPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-      <Route path="/login" element={<LoginPage />}/>
-      <Route path="/signup" element={<SignUp />}/>
+  //   <Routes>
+  //     <Route path="/" element={<HomePage />}/>
+  //     <Route path="/login" element={<LoginPage />}/>
+  //     <Route path="/signup" element={<SignUp />}/>
 
-   </Routes>
+  //  </Routes>
+  <>
+  {/* <RequestPage></RequestPage> */}
+  </>
 )}
  
 export default App;
